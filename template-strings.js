060e5df66b83cc70910f5417ef7b39a305bd85c3
Caique017 <caiquen189@gmail.com>
2023-01-09 21:08:43 -0300

    operadores e funções

---
+++ template-strings.js
@@ -0,0 +1,12 @@
+const nome = "Caique Nunes Oliveira";
+const idade = 2021-2004;
+const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é "
+// + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade}
+e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
+
---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// boolean
+
+const usuarioLogado = true
+const contapaga = true
+
+// truthy ou falsy
+
+//console.log(0 == false)
+//console.log("" == false)
+//console.log(1 == true)
+
+
+// null
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
